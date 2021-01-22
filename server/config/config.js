a55d689c6882aa814a8b0635083c695611259064
Gabriel Glez <63757997+GabrielGlez845@users.noreply.github.com>
2021-01-21 18:52:21 -0600

    T

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/muebleria';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://Gabo:JfQcoZDNxRwyEeO3@cluster0.lvj4b.mongodb.net/Muebleria';
     console.log('base de datos online')
 }
 process.env.URLDB = urlDB;
