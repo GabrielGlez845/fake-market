1816098d0b73db216f8c39c8eedc5c6d303603c6
Gabriel Glez <63757997+GabrielGlez845@users.noreply.github.com>
2021-01-22 00:07:01 -0600

    ;(

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb+srv://Gabo:JfQcoZDNxRwyEeO3@cluster0.lvj4b.mongodb.net/Muebleria';
+    urlDB = 'mongodb://localhost:27017/muebleria';
     // urlDB = 'mongodb://localhost:27017/muebleria';
 } else {
     urlDB = process.env.MONGO_URI;
